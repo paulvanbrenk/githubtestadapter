5585c4474d010171c9477acdf1051a8deee64779
Paul van Brenk <paul.van.brenk@microsoft.com>
2018-06-06 15:20:49 -0700

    initial

---
+++ CoreTestAdapter/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace CoreTestAdapter
+{
+    [FileExtension(".dll")]
+    [DefaultExecutorUri("executor://JavaScriptExecutor")]
+    public class CoreTestDiscoverer : ITestDiscoverer
+    {
+        public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+        {
+            logger.SendMessage(TestMessageLevel.Error, "DiscoverTests");
+        }
+    }
+
+    [ExtensionUri("executor://JavaScriptExecutor")]
+    public class CoreTestExecutor : ITestExecutor
+    {
+        public void Cancel()
+        {
+        }
+
+        public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            var settings = runContext.RunSettings.SettingsXml;
+            frameworkHandle.SendMessage(TestMessageLevel.Error, "RunTests");
+        }
+
+        public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            var settings = runContext.RunSettings.SettingsXml;
+            frameworkHandle.SendMessage(TestMessageLevel.Error, "RunTests");
+        }
+    }
+}
